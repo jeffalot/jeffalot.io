4378bdfc91b81945e0cabd7bf51c607bd6c184f3
Jeffalot <jeff@jeffalot.io>
2020-11-01 13:34:50 -0500

    Updating colors for navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -30,7 +30,7 @@ const StyledHeader = styled.header`
     css`
       height: var(--nav-scroll-height);
       transform: translateY(0px);
-      background-color: rgba(234, 223, 215, 0.85);
+      background-color: rgba(250, 238, 228, 0.85);
       box-shadow: 0 10px 30px -10px var(--navy-shadow);
     `};
 
